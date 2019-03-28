bb420a6fb415e50259f15cab487914c4325d7f39
Alice Gatland <unrelated@gmail.com>
2019-03-29 10:05:42 +1300

    add starting data

--- server.js
+++ server.js
@@ -2,6 +2,36 @@ const express = require('express')
 
 const server = express()
 
+const data = {
+  profiles: [
+    {
+      id: 1,
+      name: 'Raj',
+      image: 'url',
+      energy: 'medium',
+      specialSkills: [
+        'nunchucks',
+        'daydreamer'
+      ],
+      alignment: 'evil',
+      backstory: 'very prepared'
+    },
+    {
+      id: 2,
+      name: 'Amy',
+      image: 'url',
+      energy: 'high',
+      specialSkills: [
+        'cleaning',
+        'shadiness',
+        'sleeping'
+      ],
+      alignment: 'evil',
+      backstory: 'mysterious'
+    }
+  ]
+}
+
 server.get('/', (req, res) => {
   res.send('Hello world')
 })
